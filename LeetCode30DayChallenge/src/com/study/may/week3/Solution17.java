e20b6c0346a7321044b4a81ec36d9821cc02c0c6
mayur.oza30 <mayur.oza30@gmail.com>
2020-05-17 11:33:59 +0200

    Solution 17 May

---
+++ LeetCode30DayChallenge/src/com/study/may/week3/Solution17.java
@@ -0,0 +1,90 @@
+package com.study.may.week3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution17 {
+	public List<Integer> findAnagrams_slow(String s, String p) {
+
+		List<Integer> result = new ArrayList<>();
+
+		int pLen = p.length();
+		int sLen = s.length();
+
+		int[] charPArray = new int[26];
+
+		for (int i = 0; i < pLen; i++)
+			charPArray[p.charAt(i) - 'a']++;
+
+		for (int i = 0; i <= sLen - pLen; i++) {
+			String newStr = s.substring(i, pLen + i);
+			int[] newStrArray = new int[26];
+
+			for (int j = 0; j < pLen; j++)
+				newStrArray[newStr.charAt(j) - 'a']++;
+
+			if (Arrays.equals(charPArray, newStrArray))
+				result.add(i);
+
+		}
+
+		return result;
+	}
+
+	public List<Integer> findAnagrams_13ms(String s, String p) {
+		int[] letters = new int[26];
+
+		for (int i = 0; i < p.length(); i++) {
+			letters[p.charAt(i) - 'a']++;
+		}
+
+		int j = 0;
+		int rem = p.length();
+		List<Integer> result = new ArrayList<>();
+
+		for (int i = 0; i < s.length(); i++) {
+
+			while (j < s.length() && j - i < p.length()) {
+				if (letters[s.charAt(j++) - 'a']-- > 0) {
+					rem--;
+				}
+			}
+
+			if (rem == 0 && j - i == p.length())
+				result.add(i);
+
+			if (letters[s.charAt(i) - 'a']++ >= 0)
+				rem++;
+		}
+
+		return result;
+	}
+
+	public List<Integer> findAnagrams_6ms(String s, String p) {
+
+		List<Integer> list = new ArrayList<>();
+		if (s == null || s.length() == 0 || p == null || p.length() == 0)
+			return list;
+
+		int[] counter = new int[26];
+		for (int i = 0; i < p.length(); i++) {
+			counter[p.charAt(i) - 'a']++;
+		}
+
+		int left = 0, right = 0;
+		int count = p.length(); // to count if all chars found in p
+
+		while (right < s.length()) {
+			if (counter[s.charAt(right++) - 'a']-- >= 1)
+				count--;
+			if (count == 0)
+				list.add(left);
+			if (right - left == p.length() && ++counter[s.charAt(left++) - 'a'] > 0)
+				count++;
+		}
+
+		return list;
+	}
+
+}
