46f56abfb2cc30583aa662c8457df4afd808f11f
mayur.oza30 <mayur.oza30@gmail.com>
2020-06-03 12:39:20 +0200

    Update Solution 3 June

--- LeetCode30DayChallenge/src/com/study/june/week1/Solution3.java
+++ LeetCode30DayChallenge/src/com/study/june/week1/Solution3.java
@@ -46,4 +46,24 @@ public class Solution3 {
 
 		return sum;
 	}
+
+	public int twoCitySchedCost_n(int[][] costs) {
+		int N = costs.length / 2;
+		
+		int[] refund = new int[N * 2];
+		int minCost = 0, index = 0;
+		
+		for (int[] cost : costs) {
+			refund[index ++] = cost[1] - cost[0];
+			minCost += cost[0];
+		}
+		
+		Arrays.sort(refund);
+		
+		for (int i = 0; i < N; i++) {
+			minCost += refund[i];
+		}
+		
+		return minCost;
+	}
 }
