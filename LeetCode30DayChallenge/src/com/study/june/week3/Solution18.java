99e02dfa2ac37d7826509a0646b7b0e2d0f93a8b
mayur.oza30 <mayur.oza30@gmail.com>
2020-06-18 21:20:08 +0200

    Solution 18 June

---
+++ LeetCode30DayChallenge/src/com/study/june/week3/Solution18.java
@@ -0,0 +1,80 @@
+package com.study.june.week3;
+
+import java.util.Arrays;
+
+/**
+ * H-Index II
+ *
+ * 
+ * Given an array of citations sorted in ascending order (each citation is a
+ * non-negative integer) of a researcher, write a function to compute the
+ * researcher's h-index.
+ * 
+ * According to the definition of h-index on Wikipedia: "A scientist has index h
+ * if h of his/her N papers have at least h citations each, and the other N - h
+ * papers have no more than h citations each."
+ * 
+ * Input: citations = [0,1,3,5,6]
+ * 
+ * Output: 3
+ * 
+ * Explanation:
+ * 
+ * [0,1,3,5,6] means the researcher has 5 papers in total and each of them had
+ * received 0, 1, 3, 5, 6 citations respectively. Since the researcher has 3
+ * papers with at least 3 citations each and the remaining two with no more than
+ * 3 citations each, her h-index is 3.
+ * 
+ * Note:
+ * 
+ * If there are several possible values for h, the maximum one is taken as the
+ * h-index.
+ * 
+ * Follow up:
+ * 
+ * This is a follow up problem to H-Index, where citations is now guaranteed to
+ * be sorted in ascending order. Could you solve it in logarithmic time
+ * complexity?
+ * 
+ * @author mayur.p.ozardekar
+ *
+ */
+public class Solution18 {
+
+	public int hIndex(int[] citations) {
+		int n = citations.length;
+		int left = 0, right = n - 1, pivot;
+
+		while (left <= right) {
+			pivot = (left + right) / 2;
+			if (citations[pivot] == (n - pivot)) {
+				return n - pivot;
+			} else if (citations[pivot] < (n - pivot)) { // go right
+				left = pivot + 1;
+			} else { // go left
+				right = pivot - 1;
+			}
+		}
+		return n - left;
+	}
+
+	public int hIndex_n(int[] citations) {
+		Arrays.sort(citations);
+
+		int result = 0;
+
+		for (int i = citations.length - 1; i >= 0; i--) {
+
+			int cnt = citations.length - i;
+
+			if (citations[i] >= cnt) {
+				result = cnt;
+			} else {
+				break;
+			}
+		}
+
+		return result;
+	}
+
+}
