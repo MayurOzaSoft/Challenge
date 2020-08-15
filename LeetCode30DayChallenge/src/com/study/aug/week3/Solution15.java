83a2e6856cc5a6538926bc7a8186a7877f08f285
moz01 <mayur.ozardekar@hrs.com>
2020-08-15 21:10:01 +0200

    Solution 15 Aug

---
+++ LeetCode30DayChallenge/src/com/study/aug/week3/Solution15.java
@@ -0,0 +1,51 @@
+package com.study.aug.week3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution15 {
+    public int eraseOverlapIntervals_1ms(int[][] intervals) {
+
+        if(intervals.length == 0) return 0;
+
+        Arrays.sort(intervals, Comparator.comparingInt(x -> x[0]));
+
+        int count = 0;
+        int end = intervals[0][1];
+
+        for(int i = 1; i < intervals.length; i ++){
+            if(end > intervals[i][0]){
+                end = Math.min(end, intervals[i][1]);
+                count ++;
+            } else {
+                end = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
+
+    public int eraseOverlapIntervals_0ms(int[][] intervals) {
+        if(intervals.length == 0) return 0;
+
+        Arrays.sort(intervals, new MyComparator());
+
+        int end = intervals[intervals.length - 1][0];
+        int count = 1;
+
+        for(int i = intervals.length - 2; i >= 0; i --)
+            if(end >= intervals[i][1]){
+                end = intervals[i][0];
+                count ++;
+            }
+
+        return intervals.length - count;
+    }
+
+    class MyComparator implements Comparator<int[]> {
+        @Override
+        public int compare(int[] x, int[] y) {
+            return x[0] - y[0];
+        }
+    }
+}
