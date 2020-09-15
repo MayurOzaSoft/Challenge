930ee923b74cd50b88f17e851d2960121c8eb409
mayurozardekar <mayur.oza30@gmail.com>
2020-09-15 14:24:56 +0200

    Solution 15 Sept

---
+++ LeetCode30DayChallenge/src/com/study/september/week3/Solution15.java
@@ -0,0 +1,29 @@
+package com.study.september.week3;
+
+/**
+ * Length of Last Word
+ *
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+ *
+ * If the last word does not exist, return 0.
+ *
+ * Note: A word is defined as a maximal substring consisting of non-space characters only.
+ *
+ * Example:
+ *
+ * Input: "Hello World"
+ * Output: 5
+ *
+ */
+public class Solution15 {
+    public int lengthOfLastWord_1ms(String s) {
+        if(s.trim().length() == 0) return 0;
+        String[] string_array = s.split(" ");
+        return string_array[string_array.length - 1].length();
+    }
+
+    public int lengthOfLastWord_0ms(String s) {
+        if(s.trim().length() == 0) return 0;
+        return s.trim().length() - (s.trim().lastIndexOf(" ") + 1);
+    }
+}
