299fb1fb0d1fe6cf161fa16d502074bf3b4a4bc3
mayurozardekar <mayur.oza30@gmail.com>
2020-11-01 01:14:36 +0100

    Solution 31, Extra Week 5 Oct

---
+++ LeetCode30DayChallenge/src/com/study/october/week5/Solution31.java
@@ -0,0 +1,83 @@
+package com.study.october.week5;
+
+/**
+ *
+ * Recover Binary Search Tree
+ *
+ * You are given the root of a binary search tree (BST), where exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+ *
+ * Follow up: A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+ *
+ * Example 1:
+ *
+ * Input: root = [1,3,null,null,2]
+ * Output: [3,1,null,null,2]
+ * Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+ *
+ * Example 2:
+ *
+ * Input: root = [3,1,4,null,null,2]
+ * Output: [2,1,4,null,null,3]
+ * Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [2, 1000].
+ * -231 <= Node.val <= 231 - 1
+ *
+ */
+public class Solution31 {
+    TreeNode first;
+    TreeNode second;
+    TreeNode pre;
+
+    public void inorder(TreeNode root){
+        if(root==null)
+            return;
+
+        inorder(root.left);
+
+        if(pre==null){
+            pre=root;
+        }else{
+            if(root.val<pre.val){
+                if(first==null){
+                    first=pre;
+                }
+
+                second=root;
+            }
+            pre=root;
+        }
+
+        inorder(root.right);
+    }
+
+    public void recoverTree(TreeNode root) {
+        if(root==null)
+            return;
+
+        inorder(root);
+        if(second!=null && first !=null){
+            int val = second.val;
+            second.val = first.val;
+            first.val = val;
+        }
+    }
+
+    /**
+     * Definition for a binary tree node.
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
