1a062ded236f53f1e48ffd83ac2d32db6fb67cc5
mayurozardekar <mayur.oza30@gmail.com>
2020-11-22 21:56:43 +0100

    Solution 22 Nov

---
+++ LeetCode30DayChallenge/src/com/study/november/week4/Solution22.java
@@ -0,0 +1,56 @@
+package com.study.november.week4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Unique Morse Code Words
+ *
+ * International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+ *
+ * For convenience, the full table for the 26 letters of the English alphabet is given below:
+ *
+ * [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ * Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
+ *
+ * Return the number of different transformations among all words we have.
+ *
+ * Example:
+ *
+ * Input: words = ["gin", "zen", "gig", "msg"]
+ * Output: 2
+ *
+ * Explanation:
+ * The transformation of each word is:
+ * "gin" -> "--...-."
+ * "zen" -> "--...-."
+ * "gig" -> "--...--."
+ * "msg" -> "--...--."
+ *
+ * There are 2 different transformations, "--...-." and "--...--.".
+ *
+ * Note:
+ *
+ * The length of words will be at most 100.
+ * Each words[i] will have length in range [1, 12].
+ * words[i] will only consist of lowercase letters.
+ *
+ */
+public class Solution22 {
+
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morseCodes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        Set<String> morseSet = new HashSet<>();
+
+        for(String word : words){
+            StringBuilder sb = new StringBuilder();
+            char[] wordChar = word.toCharArray();
+            for(char ch : wordChar)
+                sb.append(morseCodes[ch - 'a']);
+            morseSet.add(sb.toString());
+        }
+
+        return morseSet.size();
+    }
+}
